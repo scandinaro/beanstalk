41caa7323c7c53fc30291204a2324a99dbebdaf7
Dom Scandinaro <dom@Dom.local>
2019-04-17 12:57:34 -0500

    dynamic title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dom\'s Beanstalk Test!'  });
+  res.render('index', { title: process.env.title });
 });
 
 module.exports = router;
